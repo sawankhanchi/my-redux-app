6f8716cafcb031b6593def3fbaeb6a5a8ceb63c4
sawankhanchi <sappler16@gmail.com>
2022-08-07 18:27:24 +0530

    first commit

---
+++ slices/counterSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { RootState } from '../store'
+
+export interface CounterState {
+  value: number
+}
+
+const initialState: CounterState = {
+  value: 0,
+}
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state: { value: number }) => {
+      state.value += 1
+    },
+    decrement: (state: { value: number }) => {
+      state.value -= 1
+    },
+  },
+})
+
+export const { increment, decrement } = counterSlice.actions
+
+export const selectValue = (state: RootState) => state.counter.value
+
+export default counterSlice.reducer
